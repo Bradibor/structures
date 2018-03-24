88ceb8a8d90b031ccc790763f2e0a3cfa6995293
Bradibor <just1bomb@gmail.com>
2018-03-24 10:58:09 +0300

    trash

--- src/TaskTwo/StatictTest.java
+++
@@ -1,8 +0,0 @@
-package TaskTwo;
-
-public class StatictTest implements StaticTest{
-    public static void main(String[] args) {
-        StatictTest t = new StatictTest();
-        StaticTest.keyx();
-    }
-}
